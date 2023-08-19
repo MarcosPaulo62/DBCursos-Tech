4453529e43cbbae12d7d1340126c33cf44120525
Glauber Bispo <glauber.bispo@gmail.com>
2023-08-19 20:50:09 -0300

    Ajustes do card

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { ThemeProvider } from "styled-components";
 import { ColorsTheme } from "./styles/global";
+import LandingPageCard from "./components/landing-page-card/LandingPageCard";
 
 function App() {
   const [theme, setTheme] = useState(ColorsTheme);
@@ -8,6 +9,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App"></div>
+      <LandingPageCard />
     </ThemeProvider>
   );
 }
--- src/components/landing-page-card/LandingPageCard.tsx
+++ src/components/landing-page-card/LandingPageCard.tsx
@@ -1,20 +1,42 @@
-import { StyledSpan, StyledTag, StyledTitle } from "../../styles/typography";
-import { StyledCardContainer } from "./style";
+import { StyledTag, StyledTitle } from "../../styles/typography";
+import { StyledCardContainer, StyledContainerCard } from "./style";
 
 export default function LandingPageCard() {
   return (
+    <StyledContainerCard>
     <StyledCardContainer>
+      <div>
       <StyledTitle className="title" tag="h3" fontWeight={400} fontSize="md">
-        <strong>Título do curso</strong>
-      </StyledTitle>
-      <StyledTag>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris velit
-        nisl, laoreet eu consequat quis, vulputate at risus. Duis in neque
-        venenatis augue tincidunt mollis. Duis sed euismod neque. Maecenas.
-      </StyledTag>
-      <StyledSpan className="footer-card" fontSize="md">
-        <strong>Carga horária:</strong> xx h
-      </StyledSpan>
+        <strong>Instrutores Experientes</strong>
+      </StyledTitle>      
+      <StyledTag className="card-p">
+      Nossa equipe de instrutores traz anos de experiência prática em suas respectivas áreas, oferecendo uma perspectiva valiosa sobre o mundo real.
+      </StyledTag>  
+      </div>    
     </StyledCardContainer>
+
+<StyledCardContainer>
+<div>
+<StyledTitle className="title" tag="h3" fontWeight={400} fontSize="md">
+  <strong>Abordagem Prática</strong>
+</StyledTitle>      
+<StyledTag className="card-p">
+Acreditamos na aprendizagem prática. Nossos cursos são repletos de projetos práticos que ajudam a consolidar seu aprendizado.
+</StyledTag>  
+</div>    
+</StyledCardContainer>
+
+<StyledCardContainer>
+      <div>
+      <StyledTitle className="title" tag="h3" fontWeight={400} fontSize="md">
+        <strong>Tecnologia de Ponta</strong>
+      </StyledTitle>      
+      <StyledTag className="card-p">
+      Estamos comprometidos em manter nossos cursos atualizados com as mais recentes tendências tecnológicas para prepará-lo para o sucesso no mercado de trabalho.
+      </StyledTag>  
+      </div>    
+    </StyledCardContainer>
+
+  </StyledContainerCard>
   );
 }
