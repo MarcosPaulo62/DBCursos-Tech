279ac9045a6ff8c5eddb70a31c0d5e5b91414f92
therosenstock <fabiolars2009@hotmail.com>
2023-08-20 19:22:19 -0300

    limoando app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import { ColorsTheme } from "./styles/global";
 import { Router } from "./Router";
-import { ModalCadastro } from "./Components/ModalCadastro/ModalCadastro";
 
 function App() {
   return (
     <ThemeProvider theme={ColorsTheme}>
       <div className="App">
         <Router />
-        <ModalCadastro></ModalCadastro>
       </div>
     </ThemeProvider>
   );
