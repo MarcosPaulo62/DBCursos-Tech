15e5f94b0c8868b9fcf823216ccdec6d0d24c02a
therosenstock <fabiolars2009@hotmail.com>
2023-08-20 11:21:03 -0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        
-      </div>
+      <div className="App"></div>
     </ThemeProvider>
   );
 }
