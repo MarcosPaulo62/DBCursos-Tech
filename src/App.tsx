b33afec78c9bd6a890a4ba01ddc64786c7278dda
therosenstock <fabiolars2009@hotmail.com>
2023-08-20 13:22:05 -0300

    limpando app

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <HomeAside />
-      </div>
+      <div className="App"></div>
     </ThemeProvider>
   );
 }
