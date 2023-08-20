bc54ca4db0e19cfe7ee71c35318f53fcb9c44908
diego-domajnko <diegodomajnko@gmail.com>
2023-08-20 19:11:52 -0300

    remove router private

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,7 +3,6 @@ import {Login} from "./pages/login/Login";
 import {SignUp} from "./pages/signUp/SignUp";
 import {Dashboard} from "./pages/dashboard/Dashboard";
 import Home from "./pages/home/Home";
-import {PrivateRoute} from "./pages/layout/privateRoute";
 
 export function Router() {
   return (
@@ -12,9 +11,7 @@ export function Router() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<SignUp />} />
-        <Route element={<PrivateRoute />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/layout/privateRoute.tsx
+++
@@ -1,7 +0,0 @@
-import {Outlet, Navigate} from "react-router-dom";
-
-export function PrivateRoute() {
-  const isTokenValid = localStorage.getItem("token");
-
-  return isTokenValid ? <Outlet /> : <Navigate to="/login" />;
-}
