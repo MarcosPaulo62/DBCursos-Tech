5b90d53d85359636147c22a49744d16d9d8add18
Davi Kennedy <davi_kennedy@hotmail.com>
2023-08-20 16:45:06 -0300

    update

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,12 +1,10 @@
 import { NavbarDashboard } from "../../Components/NavbarDashboard/NavbarDashboard";
-import { ConfirmExclusionCard } from "../../Components/confirmExclusionCard/ConfirmExclusionCard";
 import { DashboardWrapper } from "./styles";
 
 export function Dashboard() {
   return (
     <DashboardWrapper>
       <NavbarDashboard role="admin" />
-      <ConfirmExclusionCard />
     </DashboardWrapper>
   );
 }
