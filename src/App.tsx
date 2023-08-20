8b38abaf1822147885f9ee255f59feb3d4b3e652
MarcosPaulo62 <marcospaulosc03@gmail.com>
2023-08-20 11:20:57 -0300

    Listagem Itens concluída

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { useState } from "react";
-import { ThemeProvider } from "styled-components";
-import { ColorsTheme } from "./styles/global";
-
 function App() {
-  const [theme, setTheme] = useState(ColorsTheme);
-
   return (
-    <ThemeProvider theme={theme}>
-      <div className="App"></div>
-    </ThemeProvider>
+    <div className="App">
+      
+    </div>
   );
 }
 
