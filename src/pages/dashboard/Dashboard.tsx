b7700519f686a157e32734ae8bfa68361ce59d71
Glauber Bispo <glauber.bispo@gmail.com>
2023-08-20 17:04:56 -0300

    wip

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,13 +1,11 @@
 import { NavbarDashboard } from "../../Components/NavbarDashboard/NavbarDashboard";
-import InformationCardDashboard from "../../Components/informationCardDashboard/InformationCardDashboard";
 import { DashboardWrapper } from "./styles";
 
 export function Dashboard() {
   return (
     <>
     <DashboardWrapper>
-      <NavbarDashboard role="admin" />  
-      <InformationCardDashboard />    
+      <NavbarDashboard role="admin" /> 
     </DashboardWrapper>
     
     </>
