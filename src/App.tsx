2e24a94ccbb68d2c7a61a5b4cdfc74f077f2ae08
therosenstock <fabiolars2009@hotmail.com>
2023-08-20 19:20:12 -0300

    modal cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { ThemeProvider } from "styled-components";
 import { ColorsTheme } from "./styles/global";
 import { Router } from "./Router";
-
+import { ModalCadastro } from "./Components/ModalCadastro/ModalCadastro";
 
 function App() {
   return (
     <ThemeProvider theme={ColorsTheme}>
       <div className="App">
         <Router />
+        <ModalCadastro></ModalCadastro>
       </div>
     </ThemeProvider>
   );
