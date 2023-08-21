a0e9486ce2c8d17a3738c7e7523abf5044b39d3d
Davi Kennedy <davi_kennedy@hotmail.com>
2023-08-20 23:13:42 -0300

    update: adjustments on notFound and navbar

--- src/Components/notfound/style.ts
+++ src/Components/notfound/style.ts
@@ -1,4 +1,3 @@
-import React from "react";
 import styled from "styled-components";
 
 export const NotFoundMessage = styled.div`
@@ -6,11 +5,16 @@ export const NotFoundMessage = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  flex-grow: 1;
   height: 55vh;
   font-family: Arial, sans-serif;
-  color: ${({theme}) => theme.color.offWhite};
-  background: rgb(28,88,248);
-background: radial-gradient(circle, rgba(28,88,248,1) 0%, rgba(245,245,245,1) 100%);
+  color: ${({ theme }) => theme.color.offWhite};
+  background: rgb(28, 88, 248);
+  background: radial-gradient(
+    circle,
+    rgba(28, 88, 248, 1) 0%,
+    rgba(245, 245, 245, 1) 100%
+  );
 `;
 
 export const StatusCode = styled.h1`
@@ -21,23 +25,24 @@ export const StatusCode = styled.h1`
 export const Message = styled.p`
   font-size: 1.5rem;
   margin: 1rem 0;
-
-  
 `;
 
 export const ReturnButton = styled.button`
-    background-color: ${({theme}) => theme.color.blue};
-    font-size: 1rem;
-    height: 3.6rem;
-    border-radius: 1rem;
-    width: 15rem;
-    color: ${({theme}) => theme.color.offWhite};
-    box-shadow: rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
-    transition: 0.4s;
-    margin-top: 2rem;
+  background-color: ${({ theme }) => theme.color.blue};
+  font-size: 1rem;
+  height: 3.6rem;
+  border-radius: 1rem;
+  width: 15rem;
+  color: ${({ theme }) => theme.color.offWhite};
+  box-shadow: rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset,
+    rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset,
+    rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px,
+    rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px,
+    rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
+  transition: 0.4s;
+  margin-top: 2rem;
 
-    &:hover {
-        transform: scale(1.03);
-    }
-
-`
+  &:hover {
+    transform: scale(1.03);
+  }
+`;
--- src/pages/notfound/style.ts
+++ src/pages/notfound/style.ts
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
 export const StyledNotFoundContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    background-color: #F5F5F5;
-    
-`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  flex-direction: column;
+  background-color: #f5f5f5;
+`;
